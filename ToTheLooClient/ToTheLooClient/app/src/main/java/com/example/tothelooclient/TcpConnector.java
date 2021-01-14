e6cd62f4945b4ae6a64e234f1995b1157f78e714
Patrick Mohr <patrick-mohr1@web.de>
2021-01-14 11:37:04 +0100

    Client-Klasse umbenannt in TcpConnector und verbessert.

---
+++ ToTheLooClient/ToTheLooClient/app/src/main/java/com/example/tothelooclient/TcpConnector.java
@@ -0,0 +1,67 @@
+package com.example.tothelooclient;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TcpConnector implements Runnable {
+
+    private String messageType;
+    private String message;
+
+
+    public TcpConnector(String messageType, String message){
+        this.messageType = messageType;
+        this.message = message;
+    }
+
+
+    @Override
+    public void run() {
+        Socket socket = null;
+        String host = "192.168.2.111"; //your computer IP address
+        int port = 63570;
+
+        PrintWriter out = null;
+        BufferedReader in = null;
+        try {
+            socket = new Socket(host, port);
+            out = new PrintWriter(socket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (UnknownHostException e) {
+            Log.e("NetworkError","Don't know about host: " + host);
+        } catch (IOException e) {
+            Log.e("NetworkError","Couldn't get I/O for host: " + host);
+        }
+        String serverInput;
+        try {
+
+            out.println(message);
+            out.flush();
+
+            if(messageType.equals("TimeRequestMessage")){
+                serverInput = in.readLine();
+                Log.d("SERVER","Server: " + serverInput);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            out.close();
+            in.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
