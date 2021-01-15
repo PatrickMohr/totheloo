357ac603c0de1148d8a2a63a9bfe0cadac93143e
JonasM <JonasM@JONASM-PC.local>
2021-01-15 21:46:58 +0100

    Bewertungsactivity um Eingabefeld für Review erweitert sowie Buttonreaktion MainActivity2.java und activity_main2.xml aktualisiert

--- ToTheLooClient/ToTheLooClient/app/src/main/java/com/example/tothelooclient/MainActivity2.java
+++ ToTheLooClient/ToTheLooClient/app/src/main/java/com/example/tothelooclient/MainActivity2.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -14,6 +17,17 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        final RatingBar ratingBar2 = (RatingBar) findViewById(R.id.ratingBar2);
+        Button sendeButton = (Button) findViewById(R.id.sendeButton);
+        final TextView textviewSendRating = (TextView) findViewById(R.id.textViewSendRating);
+
+        sendeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textviewSendRating.setText("Bewertung abgeschickt!");
+            }
+        });
 
 
     }
