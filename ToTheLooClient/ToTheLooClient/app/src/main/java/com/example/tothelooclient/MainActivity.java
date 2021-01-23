df55aa33ac36aa1d6bb8f268f2d32b2539ede157
Your Name <email@email.com>
2021-01-23 09:38:18 +0100

    include fetching all backend data in MainActivity. Enabled clear traffic to have the permission to make a request

--- ToTheLooClient/ToTheLooClient/app/src/main/java/com/example/tothelooclient/MainActivity.java
+++ ToTheLooClient/ToTheLooClient/app/src/main/java/com/example/tothelooclient/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tothelooclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +10,23 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.Response;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class MainActivity extends AppCompatActivity {
     private Button mainButton;
+    private ServerConnector serverConnector;
     int ratingInt;
     private Button addButton;
     private Switch kostenSwitch;
@@ -22,6 +37,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO: put this code in an own class
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+        String url = "http://18.197.143.96:8080/api/loos";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull okhttp3.Response response) throws IOException {
+                if(response.isSuccessful()) {
+                    //TODO: format data in a way the MapActivity can handle it!
+                    String myResponse = response.body().string();
+                    System.out.println(myResponse);
+                }
+            }
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+        });
 
         RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
         pissoirSwitch = (Switch) findViewById(R.id.pissoirSwitch);
@@ -34,7 +72,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openMapsActivity();
-            }
+
+                }
         });
 
 
