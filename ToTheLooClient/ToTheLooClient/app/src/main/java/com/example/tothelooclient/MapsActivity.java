7ae9072e5f8c181e26e67c90608b393c9b768c94
Jamal Benin <Jamal@Jamals-MacBook-Pro.fritz.box>
2021-01-25 15:53:31 +0100

    workspace.xml und MapsActivity.java aktualisiert

--- ToTheLooClient/ToTheLooClient/app/src/main/java/com/example/tothelooclient/MapsActivity.java
+++ ToTheLooClient/ToTheLooClient/app/src/main/java/com/example/tothelooclient/MapsActivity.java
@@ -180,17 +180,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnInfoWindowClickListener(MyOnInfoWindowClickListener);
 
        String toilettenString = clientDatabase.getAllToiletsAsString(rating,kostenIsChecked);
+       if (toilettenString.length() != 0)
+       {
        String [] toiletten = toilettenString.split("\n");
         for (String string : toiletten) {
-            String [] parts = string.split(";");
-            String id = parts [0];
-            String name = parts [1];
-            String Lat = parts [2];
-            String Lng = parts [3];
-            String Rating = parts [4];
-             {
-                setMarker(Double.parseDouble(Lat), Double.parseDouble(Lng), name, "Bewertung:" + " " + Rating,id);
+            String[] parts = string.split(";");
+            String id = parts[0];
+            String name = parts[1];
+            String Lat = parts[2];
+            String Lng = parts[3];
+            String Rating = parts[4];
+            {
+                setMarker(Double.parseDouble(Lat), Double.parseDouble(Lng), name, "Bewertung:" + " " + Rating, id);
             }
+        }
         }
     }
 
